1a0a6ecd6558d0fcee2b499ba848426ea907f45d
Rui Abreu Ferreira <raf-ep@gmx.com>
2014-07-01 20:54:08 +0100

    Windows: Add missing OS definitions to win_defs.h
    - libuv has a uid type (uv_uid_t) use it to define uid_t
    - Include winsock2.h BEFORE windows.h (based on a comment in winsock2.h)
    - Moved BASENAMELEN definition out of os_defs.h into each os specific headers
    - Define BASENAMELEN as _MAX_PATH (from stdlib.h as 260)
    - windows.h already provides a macro RGB(), #undef it before its redefined in
      macros.h
    - Define workaround for LC_MESSAGES until libintl can be used
    - MSVC specific definitions
      - keywords - restrict, inline, __func__ (should be available in an upcoming
        update of VS2015)
      - STDERR_FILENO and STDOUT_FILENO using __fileno()
      - S_IS_DIR, S_ISREG, S_IXUSR
    - The SPECIAL_WILDCHAR is not set, its not needed in Windows

--- src/nvim/os/win_defs.h
+++ src/nvim/os/win_defs.h
@@ -1,16 +1,30 @@
 #ifndef NVIM_OS_WIN_DEFS_H
 #define NVIM_OS_WIN_DEFS_H
 
+// winsock2.h must be before windows.h - or so says Mingw
+#include <winsock2.h>
 #include <windows.h>
 #include <sys/stat.h>
+#include <io.h>
+#include <uv.h>
+#include <stdio.h>
+#include <time.h>
+#include "auto/config.h"
 
-#define TEMP_DIR_NAMES {"$TMP", "$TEMP", "$USERPROFILE", ""}
-#define TEMP_FILE_PATH_MAXLEN _MAX_PATH
+#ifndef LC_MESSAGES
+// FIXME(equalsraf): this is a hack - also used in ex_cmds2.c to build in
+// windows without libintl
+# define LC_MESSAGES LC_COLLATE
+#endif
 
 #define FNAME_ILLEGAL "\"*?><|"
 
 #define USE_CRNL
 
+// We have our own RGB macro in macros.h
+#undef RGB
+
+// For MSVC
 #ifdef _MSC_VER
 # ifndef inline
 #  define inline __inline
@@ -21,6 +35,22 @@
 # ifndef S_IXUSR
 #  define S_IXUSR S_IEXEC
 # endif
+# ifndef __func__
+#  define __func__ __FUNCTION__
+# endif
+# ifndef STDOUT_FILENO
+#  define STDOUT_FILENO _fileno(stdout)
+# endif
+# ifndef STDERR_FILENO
+#  define STDERR_FILENO _fileno(stderr)
+# endif
+#endif // _MSC_VER
+
+#define TEMP_DIR_NAMES {"$TMP", "$TEMP", "$USERPROFILE", ""}
+#define TEMP_FILE_PATH_MAXLEN _MAX_PATH
+
+#ifndef BASENAMELEN
+# define BASENAMELEN  (_MAX_PATH - 5)
 #endif
 
 #ifdef _MSC_VER
@@ -34,5 +64,7 @@ typedef SSIZE_T ssize_t;
 #  define SSIZE_MAX LONG_MAX
 # endif
 #endif
+
+typedef uv_uid_t uid_t;
 
 #endif  // NVIM_OS_WIN_DEFS_H
